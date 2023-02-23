128b67a0dd56ed3447241f23266078ff6808dff9
Leaundrae Mckinney <pytechdrae@gmail.com>
2023-02-23 08:13:24 -0500

    Slider looking decent finally

--- src/Slider/Slider.css.ts
+++ src/Slider/Slider.css.ts
@@ -4,13 +4,21 @@ import { Colors } from "../utils/helpers";
 export const styles = mergeStyleSets({
   sliderContainer: {
     height: "15%",
-    "@media screen and (min-width:1024px)": {},
-    "@media screen and (max-width:1023px)": {},
-    "@media screen and (max-width:769px)": {},
+    display:'flex',
+    flexDirection:'column',
+    "@media screen and (min-width:1024px)": {
+      rowGap:"25%"
+    },
+    "@media screen and (max-width:1023px)": {
+      rowGap:"25%"
+    },
+    "@media screen and (max-width:769px)": {
+      rowGap:"25%"
+    },
   },
-  slider:{
-    padding:"5% 0"
-  },
+  // slider: {
+  //   padding: "5% 0",
+  // },
   labelContainer: {
     display: "flex",
     justifyContent: "space-between",
@@ -18,28 +26,90 @@ export const styles = mergeStyleSets({
     "@media screen and (min-width:1024px)": {},
     "@media screen and (max-width:1023px)": {},
     "@media screen and (max-width:769px)": {
-      padding: "0 3% 2% 0%",
+      padding: "0 3% 5% 0%",
     },
   },
   label: {
-    width:"100%",
-    display:'flex',
-    alignItems:"center",
-    justifyContent:"space-between",
-    padding:"0 3% 0 0"
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: "0 3% 0 0",
   },
   value: {
     color: Colors.NeonGreen,
     "@media screen and (min-width:1024px)": {
-        fontSize: "2vw",
-
+      fontSize: "2vw",
     },
     "@media screen and (max-width:1023px)": {
-        fontSize:"4vw"
+      fontSize: "4vw",
     },
     "@media screen and (max-width:769px)": {
       fontSize: "6vw",
     },
+  },
+  rangeSlider: {
+    width: "100%",
+    position: "relative",
+    display: "flex",
+    // "-webkit-appearance": "none",
+    height: ".5em",
+    outline: "none",
+    background: Colors.VeryDarkGrey,
+    borderRadius: "25px",
+    input: {
+      width: "100%",
+      height: ".5em",
+      borderRadius: "25px",
+      background: Colors.VeryDarkGrey,
+      "input::-webkit-slider-thumb": {
+        "-webkit-appearance": "none !important",
+        "-moz-appearance": "none !important",
+        appearance: "none !important",
+        width: "1.7em",
+        height: "1.7em",
+        "z-index": 3,
+        cursor: "pointer",
+        position: "relative",
+      },
+      "input::-moz-range-thumb":{
+        // opacity:0,
+        
+        "-webkit-appearance":"none !important",
+        appearance:'none'
+      }
+    },
+  },
+  slider: {
+    "-webkit-appearance": "none !important",
+    "-moz-appearance": "none !important",
+    "z-index":5,
+    cursor:"pointer",
+    opacity:0,
+    appearance: "none !important",
+  },
+  sliderThumb: {
+    width: "1em",
+    height: "1em",
+    cursor: "pointer",
+    "z-index": 1,
+    position: "absolute",
+    background: Colors.AlmostWhite,
+    borderRadius: "50%",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%,-50%)",
+  },
+  progress: {
+    width: "50%",
+    height: ".5em",
+    borderRadius: "3px",
+    background: Colors.NeonGreen,
+    position: "absolute",
+    top: "50%",
+    transform: "translateY(-50%)",
+    left: 0,
+    pointerEvents: "none",
   },
 });
 
@@ -49,7 +119,7 @@ export const sliderStyles: ISliderStyles = {
     "::hover": {},
   },
   root: {
-    transition:'none'
+    transition: "none",
   },
   titleLabel: {
     color: Colors.AlmostWhite,
@@ -61,10 +131,10 @@ export const sliderStyles: ISliderStyles = {
       "ms-Slider-thumb": {
         border: "none !important",
       },
-      "ms-Slider-inactive":{
-        background:"initial"
+      "ms-Slider-inactive": {
+        background: "initial",
       },
-      transition:'none'
+      transition: "none",
     },
   },
   line: {},
--- src/Slider/Slider.tsx
+++ src/Slider/Slider.tsx
@@ -1,25 +1,57 @@
 import { sliderStyles, styles } from "./Slider.css";
-import {Slider} from "@fluentui/react"
-import * as React from 'react';
+import { Slider } from "@fluentui/react";
+import * as React from "react";
 
 export const SliderGroup = () => {
-    const [sliderValue,setSliderValue] = React.useState<number>(0);
-    const sliderOnChange = (value:number) => setSliderValue(value);
-    return (
-        <div className={styles.sliderContainer}>
-            <div className={styles.label}>
-                <div>Character Length</div>
-                <div className={styles.value}>{sliderValue}</div>
-            </div>
-            <Slider
-                className={styles.slider}
-                max={25}
-                value={sliderValue}
-                showValue={false}
-                onChange={sliderOnChange}
-                styles={sliderStyles}
-            />
-        </div>
-    )
-}
+  const [sliderStringValue, setSliderStringValue] = React.useState<string>("0");
+  const [sliderValue, setSliderValue] = React.useState<string>('1');
+  const sliderRef = React.useRef<HTMLInputElement>(null);
+  const progressRef = React.useRef<HTMLInputElement>(null);
+  const thumbRef = React.useRef<HTMLInputElement>(null);
+
+  React.useEffect(() => {
+    console.log(`Slider Value Changed to ${sliderValue}`);
+    const progress = progressRef.current;
+    const thumb = thumbRef.current;
+    const slider = sliderRef.current;
+    if (slider && progress && thumb) {
+        const maxVal = slider.getAttribute("max");
+        if (maxVal) {
+            const newVal = (parseInt(sliderValue) / parseInt(maxVal)) * 100 + "%";
+            progress.style.width = newVal;
+            thumb.style.left = newVal;
+        }
+    }
+  }, [sliderValue]);
+  
+  const customSlider = React.useCallback(() => {
+    const slider = sliderRef.current;
+    if (slider) {
+        setSliderValue(`${slider.value}`);
+    }
+  }, [sliderRef]);
+
+  let content = (
+    <div className={styles.sliderContainer}>
+      <div className={styles.label}>
+        <div>Character Length</div>
+        <div className={styles.value}>{sliderValue}</div>
+      </div>
+      <div className={styles.rangeSlider}>
+        <input
+          onChange={customSlider}
+          ref={sliderRef}
+          className={styles.slider}
+          type="range"
+          min={1}
+          max={60}
+          value={sliderValue}
+        />
+        <div ref={thumbRef} className={styles.sliderThumb}></div>
+        <div ref={progressRef} className={styles.progress}></div>
+      </div>
+    </div>
+  );
+  return content;
+};
 export default SliderGroup;
